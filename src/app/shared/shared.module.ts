d5bd963ec8ba727cbac42b64674258045b7d7e04
Carlos Ariel <ariels7801@gmail.com>
2019-02-14 09:36:56 -0600

    Fin de la sección 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from '@angular/core';
+// Importaciones necesarias para los link, ngfor, etc
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -6,6 +9,10 @@ import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
+    imports: [
+        RouterModule,
+        CommonModule
+    ],
     declarations: [
         HeaderComponent,
         SidebarComponent,
